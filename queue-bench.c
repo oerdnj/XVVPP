8ed7fb1a1842f673ebbc04e4aa4e13091b744e94
Ondřej Surý <ondrej@sury.org>
2024-04-29 10:46:20 +0200

    Add queue benchmark, jemalloc and couple of fixes

--- queue-bench.c
+++ queue-bench.c
@@ -8,6 +8,10 @@
 #define _GNU_SOURCE 1
 #endif
 
+#ifndef _LGPL_SOURCE
+#define _LGPL_SOURCE 1
+#endif
+
 #include <assert.h>
 #include <stdatomic.h>
 #include <threads.h>
@@ -37,7 +41,35 @@ enum {
 
 static thread_local uint32_t seed[4] = { 0 };
 
-void random_init(void) {
+#if defined(__x86_64__)
+#include <immintrin.h>
+#define pause() _mm_pause()
+#elif defined(__i386__)
+#define pause() __asm__ __volatile__("rep; nop")
+#elif defined(__ia64__)
+#define pause() __asm__ __volatile__("hint @pause")
+#elif defined(__aarch64__)
+#define pause() __asm__ __volatile__("isb")
+#elif defined(__arm__) && HAVE_ARM_YIELD
+#define pause() __asm__ __volatile__("yield")
+#elif defined(sun) && (defined(__sparc) || defined(__sparc__))
+#include <synch.h>
+#define pause() smt_pause()
+#elif (defined(__sparc) || defined(__sparc__)) && HAVE_SPARC_PAUSE
+#define pause() __asm__ __volatile__("pause")
+#elif defined(__ppc__) || defined(_ARCH_PPC) || defined(_ARCH_PWR) || defined(_ARCH_PWR2) || defined(_POWER)
+#define pause() __asm__ volatile("or 27,27,27")
+#else
+#define pause() sched_yield()
+#endif
+
+#define pause_n(iters)                                         \
+	for (size_t __pause = 0; __pause < iters; __pause++) { \
+		pause();                                       \
+	}
+
+void
+random_init(void) {
 	int r = uv_random(NULL, NULL, seed, sizeof(seed), 0, NULL);
 	assert(r == 0);
 }
@@ -96,8 +128,9 @@ struct thread_s {
 			uv_barrier_t *barrier;
 			uv_thread_cb cb;
 			uint64_t ops;
-			uint8_t rws;
 			uint64_t diff;
+			uint8_t threads;
+			uint8_t rws;
 			void *data;
 		};
 		uint8_t __padding[64];
@@ -112,11 +145,14 @@ is_write(uint8_t num, uint8_t percent) {
 }
 
 struct data {
+	uint64_t value; /* Node content */
 	struct cds_list_head head;
+	struct rcu_head rcu_head;
+	struct cds_lfq_node_rcu node;
 };
 
 static void
-mutex_list_run(void *arg0) {
+mutex_queue_run(void *arg0) {
 	struct thread_s *arg = arg0;
 	struct timespec start, end;
 	struct cds_list_head *head = arg->data;
@@ -131,13 +167,27 @@ mutex_list_run(void *arg0) {
 		if (is_write(random8(), arg->rws)) {
 			uv_mutex_lock(arg->mutex);
 			struct data *newdata = malloc(sizeof(*newdata));
-			cds_list_add(&newdata->head, head);
+			newdata->value = i;
+			cds_list_add_tail(&newdata->head, head);
 			uv_mutex_unlock(arg->mutex);
 		} else {
-			uv_mutex_lock(arg->mutex);
-			struct cds_list_head *pos, *p;
-			cds_list_for_each_safe(pos, p, head);
-			uv_mutex_unlock(arg->mutex);
+			struct data *data = NULL;
+
+			while (true) {
+				uv_mutex_lock(arg->mutex);
+				data = cds_list_first_entry(head, struct data, head);
+				if (data != NULL) {
+					cds_list_del(&data->head);
+				}
+				uv_mutex_unlock(arg->mutex);
+				if (data != NULL) {
+					break;
+				}
+				pause();
+			}
+
+			/* Do something with **data** */
+			free(data);
 		}
 	}
 
@@ -147,7 +197,7 @@ mutex_list_run(void *arg0) {
 }
 
 static void
-rwlock_list_run(void *arg0) {
+rwlock_queue_run(void *arg0) {
 	struct thread_s *arg = arg0;
 	struct timespec start, end;
 	struct cds_list_head *head = arg->data;
@@ -162,13 +212,36 @@ rwlock_list_run(void *arg0) {
 		if (is_write(random8(), arg->rws)) {
 			uv_rwlock_wrlock(arg->rwlock);
 			struct data *newdata = malloc(sizeof(*newdata));
+			newdata->value = i;
 			cds_list_add(&newdata->head, head);
 			uv_rwlock_wrunlock(arg->rwlock);
 		} else {
-			uv_rwlock_rdlock(arg->rwlock);
-			struct cds_list_head *pos, *p;
-			cds_list_for_each_safe(pos, p, head);
-			uv_rwlock_rdunlock(arg->rwlock);
+			struct cds_list_head *pos = NULL;
+			struct data *data;
+
+			while (true) {
+				uv_rwlock_rdlock(arg->rwlock);
+				pos = rcu_dereference(head->next);
+				uv_rwlock_rdunlock(arg->rwlock);
+				if (pos == NULL) {
+					pause();
+					continue;
+				}
+
+				uv_rwlock_wrlock(arg->rwlock);
+				data = cds_list_first_entry(head, struct data, head);
+				cds_list_del_rcu(&data->head);
+				uv_rwlock_wrunlock(arg->rwlock);
+
+				if (data == NULL) {
+					pause();
+					continue;
+				}
+				break;
+			}
+
+			/* Do something with **data** */
+			free(data);
 		}
 	}
 
@@ -178,7 +251,13 @@ rwlock_list_run(void *arg0) {
 }
 
 static void
-rcu_list_run(void *arg0) {
+free_data_rcu(struct rcu_head *rcu_head) {
+	struct data *data = caa_container_of(rcu_head, struct data, rcu_head);
+	free(data);
+}
+
+static void
+rcu_queue_run(void *arg0) {
 	struct thread_s *arg = arg0;
 	struct timespec start, end;
 	struct cds_list_head *head = arg->data;
@@ -194,13 +273,84 @@ rcu_list_run(void *arg0) {
 		if (is_write(random8(), arg->rws)) {
 			uv_mutex_lock(arg->mutex);
 			struct data *newdata = malloc(sizeof(*newdata));
-			cds_list_add_rcu(&newdata->head, head);
+			newdata->value = i;
+			cds_list_add_tail_rcu(&newdata->head, head);
 			uv_mutex_unlock(arg->mutex);
 		} else {
+			struct cds_list_head *pos = NULL;
+			struct data *data;
+
+			while (true) {
+				rcu_read_lock();
+				pos = rcu_dereference(head->next);
+				rcu_read_unlock();
+				if (pos == NULL) {
+					pause();
+					continue;
+				}
+
+				uv_mutex_lock(arg->mutex);
+				data = cds_list_first_entry(head, struct data, head);
+				cds_list_del(&data->head);
+				uv_mutex_unlock(arg->mutex);
+				if (data == NULL) {
+					pause();
+					continue;
+				}
+
+				break;
+			}
+
+			/* Do something with **data** */
+			call_rcu(&data->rcu_head, free_data_rcu);
+		}
+	}
+
+	time_now(&end);
+
+	arg->diff = time_microdiff(&end, &start);
+
+	rcu_unregister_thread();
+}
+
+static void
+lfqueue_run(void *arg0) {
+	struct thread_s *arg = arg0;
+	struct timespec start, end;
+	struct cds_lfq_queue_rcu *queue = arg->data;
+
+	rcu_register_thread();
+	random_init();
+
+	(void)uv_barrier_wait(arg->barrier);
+
+	time_now(&start);
+
+	for (size_t i = 0; i < arg->ops; i++) {
+		if (is_write(random8(), arg->rws)) {
+			struct data *newdata = malloc(sizeof(*newdata));
+			newdata->value = i;
+
 			rcu_read_lock();
-			struct cds_list_head *pos, *p;
-			cds_list_for_each_safe(pos, p, head);
+			cds_lfq_enqueue_rcu(queue, &newdata->node);
 			rcu_read_unlock();
+		} else {
+			struct cds_lfq_node_rcu *node = NULL;
+			struct data *data = NULL;
+
+			while (true) {
+				rcu_read_lock();
+				node = cds_lfq_dequeue_rcu(queue);
+				if (node != NULL) {
+					data = caa_container_of(node, struct data, node);
+					break;
+				}
+				rcu_read_unlock();
+				pause();
+			}
+
+			/* Do something with **data** */
+			call_rcu(&data->rcu_head, free_data_rcu);
 		}
 	}
 
@@ -222,15 +372,20 @@ usage(int argc [[maybe_unused]], char **argv) {
 
 struct test {
 	const char *name;
-	void *(*new)(void);
+	void *(*new)(size_t nelements);
 	uv_thread_cb run;
 	void (*destroy)(void *);
 };
 
 static void *
-list_new(void) {
+list_new(size_t nelements) {
 	struct cds_list_head *head = malloc(sizeof(*head));
 	CDS_INIT_LIST_HEAD(head);
+
+	for (size_t i = 0; i < nelements; i++) {
+		struct data *newdata = malloc(sizeof(*newdata));
+		cds_list_add_tail(&newdata->head, head);
+	}
 
 	return head;
 }
@@ -246,10 +401,43 @@ list_destroy(void *arg) {
 	}
 }
 
+static void *
+lfqueue_new(size_t nelements) {
+	struct cds_lfq_queue_rcu *queue = malloc(sizeof(*queue));
+
+	cds_lfq_init_rcu(queue, call_rcu);
+
+	for (size_t i = 0; i < nelements; i++) {
+		struct data *data = malloc(sizeof(*data));
+		data->value = i;
+		rcu_read_lock();
+		cds_lfq_node_init_rcu(&data->node);
+		cds_lfq_enqueue_rcu(queue, &data->node);
+		rcu_read_unlock();
+	}
+
+	return queue;
+}
+
+static void
+lfqueue_destroy(void *arg) {
+	struct cds_lfq_queue_rcu *queue = arg;
+	struct cds_lfq_node_rcu *node = NULL;
+
+	while ((node = cds_lfq_dequeue_rcu(queue)) != NULL) {
+		struct data *data = caa_container_of(node, struct data, node);
+
+		free(data);
+	}
+
+	cds_lfq_destroy_rcu(queue);
+}
+
 static struct test test_list[] = {
-	{ "mutex", list_new, mutex_list_run, list_destroy },
-	{ "rwlock", list_new, rwlock_list_run, list_destroy },
-	{ "rcu", list_new, rcu_list_run, list_destroy },
+	{ "mutex", list_new, mutex_queue_run, list_destroy },
+	{ "rwlock", list_new, rwlock_queue_run, list_destroy },
+	{ "rculist", list_new, rcu_queue_run, list_destroy },
+	{ "lfqueue", lfqueue_new, lfqueue_run, lfqueue_destroy },
 	{ NULL, NULL, NULL, NULL },
 };
 
@@ -278,7 +466,7 @@ main(int argc, char **argv) {
 		r = uv_rwlock_init(&rwlock);
 		assert(r == 0);
 
-		void *data = test->new();
+		void *data = test->new(num_ops * num_threads);
 
 		for (size_t i = 0; i < num_threads; i++) {
 			struct thread_s *t = &threads[i];
@@ -289,6 +477,7 @@ main(int argc, char **argv) {
 				.ops = num_ops,
 				.rws = rws,
 				.data = data,
+				.threads = num_threads,
 			};
 
 			r = uv_thread_create(&t->thread, test->run, t);
